47ecadcd6203bd048a71937dc9ff323d2671eae4
hdussert <hdussert@student.42.fr>
2026-08-22 16:00:57 +0200

    feat: put navbar items in an array + (wip) mobile menu

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -5,37 +5,83 @@ import {
   NavigationMenuList,
 } from "@/components/ui/navigation-menu";
 import Image from "next/image";
+import Link from "next/link";
+
+const NAVBARITEMS = [
+  {
+    name: "L'entreprise",
+    link: "/entreprise",
+  },
+  {
+    name: "Services",
+    link: "/services",
+  },
+  {
+    name: "Secteurs",
+    link: "/secteurs",
+  },
+  {
+    name: "Réalisations",
+    link: "/projets",
+  },
+];
 
 // TODO: Mobile menu
 const Navbar = () => {
   return (
-    <div className="top-0 z-20 absolute w-full flex justify-between px-4 py-2 ">
-      <Image
-        src="/SIPA-logo.png"
-        alt="logo"
-        width={155}
-        height={92}
-        className="bg-white p-2 -mt-2 -ml-4 rounded-br-2xl"
-      />
+    <div className="top-0 z-20 absolute w-full flex justify-between px-4 py-2">
+      <Link
+        href={"/"}
+        className="cursor-pointer bg-white p-2  -ml-4 rounded-r-full shadow"
+      >
+        <Image src="/SIPA-logo.png" alt="logo" width={120} height={50} />
+      </Link>
+
+      {/* DESKTOP */}
       <NavigationMenu className="hidden md:flex">
         <NavigationMenuList>
-          <NavigationMenuItem>
-            <NavigationMenuLink>L&apos;entreprise</NavigationMenuLink>
-          </NavigationMenuItem>
-
-          <NavigationMenuItem>
-            <NavigationMenuLink>Services</NavigationMenuLink>
-          </NavigationMenuItem>
-
-          <NavigationMenuItem>
-            <NavigationMenuLink>Secteurs</NavigationMenuLink>
-          </NavigationMenuItem>
-
-          <NavigationMenuItem>
-            <NavigationMenuLink>Réalisations</NavigationMenuLink>
-          </NavigationMenuItem>
+          {NAVBARITEMS.map((item, index) => (
+            <NavigationMenuItem key={index}>
+              <NavigationMenuLink href={item.link}>
+                {item.name}
+              </NavigationMenuLink>
+            </NavigationMenuItem>
+          ))}
         </NavigationMenuList>
       </NavigationMenu>
+
+      {/* MOBILE */}
+      {/* <NavigationMenu className="md:hidden">
+        <NavigationMenuList>
+          <NavigationMenuItem>
+            <NavigationMenuTrigger
+              className={buttonVariants({
+                variant: "secondary",
+                size: "icon-lg",
+              })}
+            >
+              <Menu />
+            </NavigationMenuTrigger>
+            <NavigationMenuContent>
+              <ul>
+                {NAVBARITEMS.map((item, index) => (
+                  <li key={index}>
+                    <NavigationMenuLink
+                      render={
+                        <Link href={item.link}>
+                          <div className="text-muted-foreground">
+                            {item.name}
+                          </div>
+                        </Link>
+                      }
+                    />
+                  </li>
+                ))}
+              </ul>
+            </NavigationMenuContent>
+          </NavigationMenuItem>
+        </NavigationMenuList>
+      </NavigationMenu> */}
     </div>
   );
 };
--- components/ui/navigation-menu.tsx
+++ components/ui/navigation-menu.tsx
@@ -2,7 +2,6 @@ import { NavigationMenu as NavigationMenuPrimitive } from "@base-ui/react/naviga
 import { cva } from "class-variance-authority";
 
 import { cn } from "@/lib/utils";
-import { ChevronDownIcon } from "lucide-react";
 
 function NavigationMenu({
   align = "start",
@@ -71,10 +70,6 @@ function NavigationMenuTrigger({
       {...props}
     >
       {children}{" "}
-      <ChevronDownIcon
-        className="relative top-px ml-1 size-3 transition duration-300 group-data-popup-open/navigation-menu-trigger:rotate-180 group-data-open/navigation-menu-trigger:rotate-180"
-        aria-hidden="true"
-      />
     </NavigationMenuPrimitive.Trigger>
   );
 }
